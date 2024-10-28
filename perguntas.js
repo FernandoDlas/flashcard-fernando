1263aa44607c3866f43db119db9793ebc20b068e
FernandoDlas <105080873+FernandoDlas@users.noreply.github.com>
2024-10-28 11:29:39 -0300

    functiom

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criacartao(
+      'programação'
+      'o que é HTML?',
+      'é uma linguagem de marcação de hipertexto'
+)
+criacartao(
+     'programação',
+     'o que é HTML?'
+     'é uma linguagem de marcação de hipertexto'
+)
+criacartao(
+     'programação',
+     'o que é HTML?',
+     'é uma linguagem de marcação de hipertexto'
+)
